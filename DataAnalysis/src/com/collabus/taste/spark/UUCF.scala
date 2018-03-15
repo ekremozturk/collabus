012665771513dabdcf3e7639417a157c01f66f3b
ekremozturk <ekrem.ozturk@boun.edu.tr>
2018-03-15 22:57:08 +0300

    UUCF

--- DataAnalysis/src/com/collabus/taste/spark/UUCF.scala
+++ DataAnalysis/src/com/collabus/taste/spark/UUCF.scala
@@ -6,8 +6,10 @@ import org.apache.log4j._
 import java.io._
 import org.apache.spark.sql._
 import org.apache.spark.sql.functions._
+import scala.math.sqrt
+import scala.collection.mutable.ListBuffer
 
-object NormalizePlayCount {
+object UUCF {
   
   case class Triplet(userID: String, songID: String, playCount: Int)
   
@@ -35,23 +37,23 @@ object NormalizePlayCount {
   
   def main(args: Array[String]) {
   
-  Logger.getLogger("org").setLevel(Level.ERROR)
+    Logger.getLogger("org").setLevel(Level.ERROR)
     
-    // Use new SparkSession interface in Spark 2.0
     val spark = SparkSession
       .builder
       .appName("SparkSQL")
       .master("local[*]")
       .getOrCreate()
-    
-    // un: unnormalized, n: normalized, upm: user play mean  
       
-    //Read the file
-    val unLines = spark.sparkContext.textFile("/Users/ekrem/No-cloud/datasets4senior/train_triplets_echonest.txt")
+    val pathUN = "/Users/ekrem/No-cloud/datasets4senior/train_triplets_echonest.txt"
     
-    val upmLines = spark.sparkContext.textFile("/Users/ekrem/No-cloud/datasets4senior/echonest_user_play_mean.txt")
-    
+    val pathUPM = "/Users/ekrem/No-cloud/datasets4senior/echonest_user_play_mean.txt"  
+      
+    val unLines = spark.sparkContext.textFile(pathUN)
+
     val unTriplets = unLines.map(parseTriplet)
+    
+    val upmLines = spark.sparkContext.textFile(pathUPM)
     
     val upmTriplets = upmLines.map(parseFourTuple)
     
@@ -67,13 +69,43 @@ object NormalizePlayCount {
     
     val triplets = spark.sql("SELECT * FROM unTriplets")
     
+    val I = 5 //common song subset min limit
+    
+    val k = 30 //k-NN
+    
     val joined = triplets.join(userMean, "userID")
     
-    val nTriplets = joined.withColumn("playCount", $"playCount" - $"mean")
+    val nTriplets = joined.withColumn("playCount", $"playCount" - $"mean").drop("mean")
     
-    println(nTriplets.show(200))
+    val nTripletsOnlyLiked = nTriplets.filter(nTriplets("playCount") > 0)
     
+    println(nTriplets.show)
+
     spark.stop()
+
+  }
+  
+  def pearsonCorr(r_u: Array[Row], r_v: Array[Row]): Double = {
+    
+    //Assuming the play counts are normalized
+    
+    var cov = 0.0
+    
+    var var_u = 0.0
+    
+    var var_v = 0.0
+    
+    r_u.foreach(row_u => r_v.foreach(row_v => cov += row_u.getDouble(0)*row_v.getDouble(0)))
+    
+    r_u.foreach(row_u => var_u += row_u.getDouble(0)*row_u.getDouble(0))
+    
+    r_v.foreach(row_v => var_v += row_v.getDouble(0)*row_v.getDouble(0))
+    
+    return cov/(sqrt(var_u)*sqrt(var_v))
+  }
+  
+  def numOfDistinct(df: DataFrame, field: String): Long = {
+    return df.select(field).distinct().count()
   }
   
 }
