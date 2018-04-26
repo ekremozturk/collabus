4e69693cbad7d7376a78f6cea016a7e57c96dc19
ekremozturk <ekrem.ozturk@boun.edu.tr>
2018-04-26 15:19:59 +0300

    rec for single user done

--- taste_main_knn.py
+++ taste_main_knn.py
@@ -9,6 +9,7 @@ Created on Wed Apr 25 11:23:24 2018
 import numpy as np
 import pandas as pd
 import taste_main_script as fn
+from time import time
 
 from math import log, exp
 from numpy.linalg import norm
@@ -106,12 +107,45 @@ def u_pred_i(userID, songID, M, k):
   
   return sum_pred/norm(w_i)
 
-def form_predictions(userID):
-  R_pred = R
-  
-k = exp(u_pred_i('00106661302d2251d8bb661c91850caa65096457', 'SOCDDDO12AC4689F56', M, 30)-1)  
-  
+R_pred = np.zeros((userIDs.size, songIDs.size))
 
+def form_user_prediction(userID, k):
+  user_idx = user_dict[userID]
+  user_array = R[user_idx,:]
+  user_pred = []
+  for idx, item in enumerate(user_array):
+    songID = songIDs[idx]
+    if item == 0:
+      r_pred = u_pred_i(userID, songID, M, k)
+    else:
+      r_pred = 0
+    user_pred.append(r_pred)
+  return user_pred
+
+ 
+#kdg = form_user_prediction('00106661302d2251d8bb661c91850caa65096457', 30)
+
+
+def form_R_pred(k):
+  R_pred = []
+  for uid in userIDs:
+    preds = form_user_prediction(uid, k)
+    R_pred.append(preds)
+    print(preds)
+  return R_pred
+    
+def recommend_user(userID, n):
+  user_pred = form_user_prediction(userID, 30)
+  rec = []
+  for lc, song_id in zip(user_pred, songIDs):
+    if(lc != 0):
+      rec.append((lc, song_id))
+  rec = sorted(rec, key=lambda x: x[0], reverse=True)
+  return rec[0:n]
+
+start_time = time() 
+ggg = recommend_user('00106661302d2251d8bb661c91850caa65096457', 20)
+elapsed_time = time()-start_time
 #seha
 # Precision metric kesin, daha sonra mean average precision
 # Başka metric varsa onlar da
@@ -125,7 +159,7 @@ k = exp(u_pred_i('00106661302d2251d8bb661c91850caa65096457', 'SOCDDDO12AC4689F56
 #herkes
 # Evaluation için de cross validation
 # gridsearchcv 
-  
+# msd h5 summary
   
   
   
